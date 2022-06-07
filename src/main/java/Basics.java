4a9b39902332aa6456300ed745f778f9c81d400d
Santiago Arizabaleta <Santiago.Arizabaleta@endava.com>
2022-06-07 10:30:44 -0500

    feat: add validations for post, put and get results

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -1,35 +1,49 @@
+import files.bodyData;
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
 
 import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
 
 public class Basics {
     public static void main(String[] args) {
-// validate if Add place API is working as expected
 
-        //given - all input details
-        // when - Submit the API - resource, http method
-        // there - validate the response
+        //Create new Place
         RestAssured.baseURI = "https://rahulshettyacademy.com";
-        given()
+        String response = given()
                 .log().all()
                 .queryParam("key", "qaclick123").header("Content-Type", "application/json")
-                .body("" +
-                        "{\n" +
-                        "    \"location\": {\n" +
-                        "        \"lat\": -38.383494,\n" +
-                        "        \"lng\": 33.427362\n" +
-                        "    },\n" +
-                        "    \"accuracy\": 50,\n" +
-                        "    \"name\": \"Frontline house\",\n" +
-                        "    \"phone_number\": \"(+91) 983 893 3937\",\n" +
-                        "    \"address\": \"29, side layout, cohen 09\",\n" +
-                        "    \"types\": [\n" +
-                        "        \"shoe park\",\n" +
-                        "        \"shop\"\n" +
-                        "    ],\n" +
-                        "    \"website\": \"http://google.com\",\n" +
-                        "    \"language\": \"French-IN\"\n" +
-                        "}").when().post("maps/api/place/add/json")
-                .then().log().all().assertThat().statusCode(209);
+                .body(bodyData.AddPlace())
+                .when().post("maps/api/place/add/json")
+                .then().log().all().assertThat()
+                .statusCode(200)
+                .body("status", equalTo("OK"))
+                .body("scope", equalTo("APP"))
+                .extract().response().asString();
+
+        JsonPath js = new JsonPath(response); //Parsing Json
+        String place_id = js.getString("place_id");
+        String newAddress = "Summer Walk, Africa";
+
+        //Update address place
+        given().log().all().queryParam("key", "qaclick123").header("Content-Type", "application/json")
+                .body(bodyData.UpdateAddress(place_id, newAddress))
+                .when().put("maps/api/place/update/json")
+                .then().assertThat().log().all()
+                .statusCode(200)
+                .body("msg", equalTo("Address successfully updated"));
+
+        //Get place
+        given().log().all().queryParam("key", "qaclick123").queryParam("place_id", "3880efd06f247214e60f940f392f0e11")
+                .when().get("maps/api/place/get/json")
+                .then().assertThat().log().all()
+                .statusCode(200)
+                .body("address", equalTo("Summer Walk, Africa"))
+                .body("name", equalTo("Frontline house"))
+                .body("phone_number", equalTo("(+91) 983 893 3937"))
+                .body("types", equalTo("shoe park,shop"))
+                .body("website", equalTo("http://google.com"))
+                .body("language", equalTo("French-IN"))
+                .body("accuracy", equalTo("50"));
     }
 }
